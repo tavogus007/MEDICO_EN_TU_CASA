dcfdf7a318a024fca565f4979d28688af479ae24
tavogus007 <gustavog095065@gmail.com>
2025-03-07 16:01:32 -0400

    Basic Nest structure, complete version, adding services file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,9 @@ import { AppService } from './app.service';
 import { AgendaController } from './controllers/mec_agenda/agenda/agenda.controller';
 import { FormularioController } from './controllers/mec_agenda/formulario/formulario.controller';
 import { HistorialController } from './controllers/mec_historial/historial.controller';
+import { AgendaService } from './services/agenda.service';
+import { RutaService } from './services/ruta.service';
+import { HistorialService } from './services/historial.service';
 
 @Module({
   imports: [],
@@ -13,6 +16,6 @@ import { HistorialController } from './controllers/mec_historial/historial.contr
     FormularioController,
     HistorialController,
   ],
-  providers: [AppService],
+  providers: [AppService, AgendaService, RutaService, HistorialService],
 })
 export class AppModule {}
