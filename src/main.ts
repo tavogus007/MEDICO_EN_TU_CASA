12bc9d0ce1535e2fa9e0edee7b814bc124d4c29b
tavogus007 <gustavog095065@gmail.com>
2025-03-21 16:59:20 -0400

    Fixing main file again

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, documentFactory);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
